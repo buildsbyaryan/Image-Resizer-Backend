1a9c2edb6cf3731505b0d1061de731f4d9ac282c
Sharmaaryan555 <sharmaaryan55555@gmail.com>
2025-10-08 22:21:46 +0530

    changed port config

--- api/index.js
+++ api/index.js
@@ -26,5 +26,8 @@ app.post("/resize", upload.single("image"), async (req, res) => {
   }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
